c7ecf6f9ddc6bbe96a923277c8a18b1d1f2f3c30
CaptainSerbs <sere.makinen@student.businesscollege.fi>
2017-11-03 13:38:44 +0200

    Basic movement and followcamera

--- SJMgameprojectstuff/Assets/Scripts/MovementController.cs
+++ SJMgameprojectstuff/Assets/Scripts/MovementController.cs
@@ -16,17 +16,44 @@ public class MovementController : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        }
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
         }
         if (Input.GetKey(KeyCode.W))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveSpeed);
         }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        }
         if (Input.GetKey(KeyCode.S))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -moveSpeed);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        }
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            // Suunta on oikea
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            // Suunta on vasemmalle
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
 }
