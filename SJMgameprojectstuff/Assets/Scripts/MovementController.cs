b863d3e26748ef104bdf6b62c7898ba241cbcbf5
dankdalff <markus.7@hotmail.com>
2018-02-13 19:00:52 +0200

    Hands and movement
    Hands are now following the mouse and movement was changed

--- SJMgameprojectstuff/Assets/Scripts/MovementController.cs
+++ SJMgameprojectstuff/Assets/Scripts/MovementController.cs
@@ -3,48 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour {
-    public float moveSpeed = 0;
+    
+    [SerializeField]
+    private float moveSpeed = 0;
+    private Quaternion currentAngle;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveSpeed);
-        }
-        else if (Input.GetKeyUp(KeyCode.W))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -moveSpeed);
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
-        }
 
+    [SerializeField]
+    [Range(10f, 100f)]
+    private float rotateSpeed = 10f;
+
+    [SerializeField]
+    private GameObject lArm;
+    [SerializeField]
+    private GameObject rArm;
+
+    // Update is called once per frame
+    void Update () {
+
+        if(GetComponent<Rigidbody2D>().velocity != new Vector2(0, 0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveSpeed * Input.GetAxis("Vertical"));
+            } else if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, moveSpeed * Input.GetAxis("Vertical"));
+            }
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
         {
             // Suunta on oikea
@@ -56,4 +43,26 @@ public class MovementController : MonoBehaviour {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawLine(ray.origin, ray.direction * 10000, Color.cyan);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10000);
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit!");
+            Debug.Log(hit.collider.name);
+
+            Vector3 hitPoint = hit.point;
+
+            Vector3 targetDir = hitPoint - transform.position;
+
+            float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+            Quaternion targetAngle = Quaternion.AngleAxis(angle, Vector3.forward);
+            lArm.transform.rotation = Quaternion.Slerp(lArm.transform.rotation, targetAngle, rotateSpeed * Time.deltaTime);
+            rArm.transform.rotation = Quaternion.Slerp(rArm.transform.rotation, targetAngle, rotateSpeed * Time.deltaTime);
+        }
+    }
+
 }
